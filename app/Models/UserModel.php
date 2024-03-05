d8e962015c11c8ddfdb0a6d3c4450857508842b6
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-03-05 09:16:45 +0700

    Praktikum1(Jobsheet4)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,8 @@ class UserModel extends Model
 
     protected $table = 'm_users';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+    //@var array
+    //protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
