7484f2190a01efc50c379dd44b44baeb1b3c3868
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-03-16 23:48:55 +0700

    Praktikum 3 (Jobsheet5)

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::post('user/tambah_simpan/', [UserController::class, 'tambah_simpan'])->na
 Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
 
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+
