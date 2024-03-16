d76d2c66a103a21d16bbe7eddc565913207e0d8e
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-03-16 22:43:38 +0700

    Praktikum 2 (Jobsheet5)

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-
 class KategoriSeeder extends Seeder
 {
     /**
@@ -15,30 +14,44 @@ class KategoriSeeder extends Seeder
     public function run(): void
     {
         $data = [
+            // [
+            //     'kategori_id' => 1,
+            //     'kategori_kode' => 'KTG001',
+            //     'kategori_nama' => 'Hijab',
+            // ],
+            // [
+            //     'kategori_id' => 2,
+            //     'kategori_kode' => 'KTG002',
+            //     'kategori_nama' => 'Dress',
+            // ],
+            // [
+            //     'kategori_id' => 3,
+            //     'kategori_kode' => 'KTG003',
+            //     'kategori_nama' => 'Kemeja',
+            // ],
+            // [
+            //     'kategori_id' => 4,
+            //     'kategori_kode' => 'KTG004',
+            //     'kategori_nama' => 'Celana',
+            // ],
+            // [
+            //     'kategori_id' => 5,
+            //     'kategori_kode' => 'KTG005',
+            //     'kategori_nama' => 'Aksesoris',
+            // ],
             [
                 'kategori_id' => 1,
-                'kategori_kode' => 'KTG001',
-                'kategori_nama' => 'Hijab',
+                'kategori_kode' => 'CML',
+                'kategori_nama' => 'Cemilan',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'kategori_id' => 2,
-                'kategori_kode' => 'KTG002',
-                'kategori_nama' => 'Dress',
-            ],
-            [
-                'kategori_id' => 3,
-                'kategori_kode' => 'KTG003',
-                'kategori_nama' => 'Kemeja',
-            ],
-            [
-                'kategori_id' => 4,
-                'kategori_kode' => 'KTG004',
-                'kategori_nama' => 'Celana',
-            ],
-            [
-                'kategori_id' => 5,
-                'kategori_kode' => 'KTG005',
-                'kategori_nama' => 'Aksesoris',
+                'kategori_kode' => 'MNR',
+                'kategori_nama' => 'Minuman Ringan',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
         ];
         DB::table('m_kategoris')->insert($data);
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/ha
 
 Route::post('user/tambah_simpan/', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
 Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
+
+Route::get('/kategori', [KategoriController::class, 'index']);
