d617b1d17d7ee8c40a80d51188f4d788b75c9b2c
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-04-23 10:47:41 +0700

    Praktikum 1 (Jobsheet10)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,55 +2,74 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+// use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model implements Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
 
-    protected $table = 'm_users';       // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
-    //@var array
+    protected $table = 'm_users';
+    protected $primaryKey = 'user_id';
+
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    //protected $fillable = ['level_id', 'username', 'nama'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-
-    public function getAuthIdentifierName()
-    {
-        return 'user_id';
-    }
-
-    public function getAuthIdentifier()
-    {
-        return $this->user_id;
-    }
-
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    public function getRememberToken()
-    {
-        return $this->remember_token;
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
 
 }
+
+// class UserModel extends Model implements Authenticatable
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_users';       // Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+//     //@var array
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+//     //protected $fillable = ['level_id', 'username', 'nama'];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+
+//     public function getAuthIdentifierName()
+//     {
+//         return 'user_id';
+//     }
+
+//     public function getAuthIdentifier()
+//     {
+//         return $this->user_id;
+//     }
+
+//     public function getAuthPassword()
+//     {
+//         return $this->password;
+//     }
+
+//     public function getRememberToken()
+//     {
+//         return $this->remember_token;
+//     }
+
+//     public function setRememberToken($value)
+//     {
+//         $this->remember_token = $value;
+//     }
+
+//     public function getRememberTokenName()
+//     {
+//         return 'remember_token';
+//     }
+
+// }
