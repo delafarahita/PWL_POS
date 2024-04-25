8d813243cd5b5243b6cf3f4aacaea950c6b44724
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-04-25 13:29:34 +0700

    Tugas (Jobsheet 10)

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\UserModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = UserModel::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function show(UserModel $user)
+    {
+        return UserModel::find($user);
+    }
+
+    public function update(Request $request, UserModel $user)
+    {
+        $user->update($request->all());
+        return UserModel::find($user);
+    }
+
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
